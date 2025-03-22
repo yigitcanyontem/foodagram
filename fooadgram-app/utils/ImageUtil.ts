ce28a753e7daf1d52e0969377583f3e4b78f6f5c
Yiğit Can Yöntem <yigitcanyontem@outlook.com>
2025-03-22 23:32:27 +0300

    formdata fixed

--- fooadgram-app/utils/ImageUtil.ts
+++ fooadgram-app/utils/ImageUtil.ts
@@ -1,9 +1,11 @@
 export const ImageUtil = {
-    getFormDataFrom: (fileUri: string, fileName: string): string => {
+    getFormDataFrom: (fileUri: string, fileName: string): FormData => {
         fileUri = fileUri.replace('file:/data', 'file:///data');
 
         let type = fileUri.substring(fileUri.lastIndexOf(".") + 1);
         const formData = new FormData();
-        formData.append('file', {uri: fileUri, name: fileName, type: `image/${type}`} as any)
+        formData.append('file', { uri: fileUri, name: fileName, type: `image/${type}` } as any);
+
+        return formData;
     }
 }
